77682d3cdbe2fb2d8b68cabdae97fc5027259f05
T10H <107046775+T10H@users.noreply.github.com>
2022-09-02 09:38:51 -0300

    Add files via upload

---
+++ .js
@@ -0,0 +1,12 @@
+var images = ["https://http2.mlstatic.com/D_NQ_NP_738284-MLB44458593400_122020-W.jpg","https://i.pinimg.com/474x/8d/ee/de/8deede6888af1f1be092c160e76a77f0.jpg","https://i.pinimg.com/originals/3a/2f/62/3a2f62cbc8115270192a2d75f861b794.jpg","https://static.vecteezy.com/ti/vetor-gratis/p1/5203676-cartoon-engracado-garotinho-de-pe-gr%C3%A1tis-vetor.jpg"]
+var names  = ["Álbun de Família","Daniel Hiramatsu","Evelini Hiramatsu","Thomás Hiramatsu","Bernardo Hiramatsu",
+{
+    i++;
+    var numbers = 5
+}
+{
+    i = 0
+}
+    var updatedImage = images[i];
+    var updatedName = names[i];
+    document.getElementById("familyMemberImage").src = updatedImage
